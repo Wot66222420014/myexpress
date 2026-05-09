fafef560820767e7a9cd728e8b886385ecadc3ff
Wot66222420014 <151432867+Wot66222420014@users.noreply.github.com>
2026-05-09 18:00:34 +0700

    fix

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
-// index.js
+//Lastest version of line bot sdk: https://www.npmjs.com/package/@line/bot-sdk
 require('dotenv').config();
+
 const express = require('express');
 const line = require('@line/bot-sdk');
 
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
 
-// ตั้งค่าจาก LINE Developers Console
+// create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
-  channelSecret: process.env.CHANNEL_SECRET || ""
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
-app.use('/webhook', line.middleware(config));
-
-// รับ webhook
-app.post('/webhook', (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result));
+// create LINE SDK client
+const client = line.LineBotClient.fromChannelAccessToken({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 });
 
-// ตอบกลับข้อความ
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: `คุณพิมพ์ว่า: ${event.message.text}`
+  // create an echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo],
   });
 }
-
-const client = new line.messagingApi.MessagingApiClient(config);
 
 // เพิ่ม GET Method
 app.get('/', (req, res) => {
   res.send('hello world, Aekkapot');
 });
 
-const PORT = process.env.PORT || 3009;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+// listen on port
+const port = process.env.PORT || 3009;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
